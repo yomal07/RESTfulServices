c5e5a02074f845505b0e73120c2fde527bfb70af
Yomal Bandara <yomalb07@gmail.com>
2022-04-16 15:06:27 +0530

    Operation to read a URL parameter

--- src/main/java/com/Hello.java
+++ src/main/java/com/Hello.java
@@ -1,7 +1,8 @@
 package com;
 
 import javax.ws.rs.GET; 
-import javax.ws.rs.Path; 
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces; 
 import javax.ws.rs.core.MediaType; 
 
@@ -9,11 +10,13 @@ import javax.ws.rs.core.MediaType;
 public class Hello {
 	
 	@GET
-	@Path("/") 
+	@Path("/{name}") 
 	@Produces(MediaType.TEXT_PLAIN) 
-	public String hello() 
+	public String helloName(@PathParam("name") String name) 
 	 { 
-		return "Hello world."; 
-	 } 
+		return "Hello " + name; 
+	 }
+	
+	
 
 }
